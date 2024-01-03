a06bcc9df2af60efbc12906cc4b14306474302e4
ueeclara <dias.ana@escolar.ifrn.edu.br>
2024-01-03 10:52:54 -0300

    corrigindo requisição

--- crud/login.js
+++ crud/login.js
@@ -1,9 +1,18 @@
 alert("email: adm | senha: adm")
 
 async function logar(){
+    const email = document.getElementById("email").value
+    const senha = document.getElementById("senha").value
+
     try {
-        const respostaServer = await fetch("localhost:3000/login")
-        const token = respostaServer.json()
+        const respostaServer = await fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({email, senha})
+        })
+        const token = await respostaServer.json()
 
         if(!token.token){
             alert("Você não tem permissão para acessar essa página")
