0c11cb5fa8fc229577130a952c73dcab69ac97f6
ueeclara <dias.ana@escolar.ifrn.edu.br>
2024-01-02 22:47:49 -0300

    arquivos para login

---
+++ crud/login.js
@@ -0,0 +1,15 @@
+async function logar(){
+    try {
+        const respostaServer = await fetch("localhost:3000/login")
+        const token = respostaServer.json()
+
+        if(!token){
+            alert("Você não tem permissão para acessar essa página")
+        }else{
+            localStorage.setItem("token", token)
+            window.location.href = "./logout.html";
+        }
+    } catch (error) {
+        alert(`Erro ao logar - ${error}`)
+    }
+}
