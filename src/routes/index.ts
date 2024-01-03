235a8b786001417ac5f6ce46466a63fc198caf71
ueeclara <dias.ana@escolar.ifrn.edu.br>
2024-01-03 10:53:04 -0300

    rotas de produtos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import user from "./userRoutes"
 import login from "./loginRoutes"
+import produtos from "./produtoRoutes"
 import {Router} from "express"
 import auth from "../middlewares/auth-middleware" 
 
 const routes = Router()
 
-routes.use(login , auth, user)
+routes.use(login , auth, user, produtos)
 
 export default routes
