45151534477c14a2d5b4a22fb3daaab35d4a5f89
ueeclara <dias.ana@escolar.ifrn.edu.br>
2024-01-02 22:30:20 -0300

    app

---
+++ app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import routes from "./src/routes/index"
+
+const app = express()
+
+app.use(express.json(), routes)
+const porta = 3000
+
+app.listen(porta, () => console.log("Server is running on port 3333"))
